df967c4d337063183eda67d91460ef059ac49921
St1ckman67 <stevehawkes67@gmail.com>
2018-09-18 19:51:10 +0100

    Corrected comments

--- main.py
+++ main.py
@@ -67,15 +67,15 @@ def print_win(winner, loser):
 
 
 def rpsls(player_choice): 
-     # print out the message for the player's choice
+    # print out the message for the player's choice
     print("\nPlayer chooses " + player_choice)
 
     # convert the player's choice to player_number using the function name_to_number()
     player_number = name_to_number(player_choice)
-    #print(player_number)
+
     # compute random guess for comp_number using random.randrange()
     comp_number = random.randrange(0,5)
-    #print(comp_number)
+
     # convert comp_number to comp_choice using the function number_to_name()
     comp_choice = number_to_name(comp_number)
 
@@ -83,11 +83,9 @@ def rpsls(player_choice):
     print("Computer chooses " + comp_choice)
 
     # compute difference of comp_number and player_number modulo five
-    #difference = player_number - comp_number
     result = (player_number - comp_number) % 5
 
-    #print(result)
-    # use if/elif/else to determine winner, print winner message
+    # determine winner, print winner message
     if result == 0:
         print("It's a tie")
     elif result in range(1,3) :
@@ -106,6 +104,3 @@ rpsls("Spock")
 rpsls("paper")
 rpsls("lizard")
 rpsls("scissors")
-
-# always remember to check your completed program against the grading rubric
-
